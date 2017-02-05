26239ac833df5381f7bd0f2b86e4c963cc2dba3b
OCEAN\user <panda6412@gmail.com>
2017-02-05 21:14:07 +0800

    no message

---
+++ app/src/main/java/com/ocean/babycar/BabyCartReservationActivity.java
@@ -0,0 +1,31 @@
+package com.ocean.babycar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class BabyCartReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baby_cart_reservation);
+
+        initToolBar();
+
+
+    }
+
+    private void initToolBar() {
+        Toolbar toolbar = (Toolbar)findViewById(R.id.id_toolbar);
+
+        setSupportActionBar(toolbar);
+        TextView tv_toolbar_title = (TextView)toolbar.findViewById(R.id.tv_toolbar_title);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        tv_toolbar_title.setText("搭乘人數");
+    }
+}
